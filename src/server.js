929329063cef0f5c0f7a7b23a9ae42180778ab38
d-levchenko <dl.qubb@gmail.com>
2026-07-14 23:48:23 +0300

    Added middleware; added routes for requests

--- src/server.js
+++ src/server.js
@@ -1,7 +1,29 @@
 import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+import 'dotenv/config';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT ?? 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
 
 app.use((req, res, next) => {
   console.log(`Time: ${new Date().toLocaleString()}`);
@@ -9,11 +31,21 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello, World!' });
+  res.status(200).json({ message: 'Hello world!' });
 });
 
-app.get('/test-error', (req, res) => {
-  throw new Error('Something went wrong');
+app.get('/notes', (req, res) => {
+  res.json({ message: 'Retrieved all notes' });
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  const noteId = req.params.noteId;
+
+  res.json({ message: `Retrieved note with ID: ${noteId}` });
+});
+
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
 });
 
 app.use((req, res) => {
@@ -25,6 +57,18 @@ app.use((err, req, res, next) => {
   res.status(500).json({
     message: 'Internal Server Error',
     error: err.message,
+  });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+
+  const isProd = process.env.NODE_ENV === 'production';
+
+  res.status(500).json({
+    message: isProd
+      ? 'Something went wrong. Please try again later.'
+      : err.message,
   });
 });
 
