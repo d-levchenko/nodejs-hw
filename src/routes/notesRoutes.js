a5ad8de7d9feda768ba9794f1a3b1592d9d2c548
d-levchenko <dl.qubb@gmail.com>
2026-07-24 00:07:36 +0300

    Added note model; added routes for notes

---
+++ src/routes/notesRoutes.js
@@ -0,0 +1,28 @@
+import { Router } from 'express';
+import { Note } from '../models/note.js';
+
+const router = Router();
+
+router.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello world!' });
+});
+
+router.get('/notes', async (req, res) => {
+  const notes = await Note.find();
+
+  res.status(200).json(notes);
+});
+
+router.get('/notes/:noteId', async (req, res) => {
+  const { noteId } = req.params;
+
+  const note = await Note.findById(noteId);
+
+  if (!note) {
+    return res.status(404).json({ message: 'Note not found' });
+  }
+
+  res.status(200).json(note);
+});
+
+export default router;
--- src/server.js
+++ src/server.js
@@ -7,6 +7,8 @@ import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 
+import notesRoutes from './routes/notesRoutes.js';
+
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
@@ -19,28 +21,14 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello world!' });
-});
+app.use(notesRoutes);
 
-app.get('/notes', (req, res) => {
-  res.json({ message: 'Retrieved all notes' });
-});
-
-app.get('/notes/:noteId', (req, res) => {
-  const noteId = req.params.noteId;
-
-  res.json({ message: `Retrieved note with ID: ${noteId}` });
-});
-
-app.get('/test-error', () => {
-  throw new Error('Simulated server error');
-});
-
+// 404
 app.use((req, res) => {
   notFoundHandler(req, res);
 });
 
+// 500
 app.use((err, req, res, next) => {
   errorHandler(err, req, res, next);
 });
