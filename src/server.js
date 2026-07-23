afbd29de316a00040f45067bad9691e215dfff6a
d-levchenko <dl.qubb@gmail.com>
2026-07-23 23:52:59 +0300

    Splitting files on modules; added middleware;

--- src/server.js
+++ src/server.js
@@ -1,32 +1,18 @@
 import express from 'express';
 import cors from 'cors';
-import pino from 'pino-http';
-import { connectMongoDB } from './db/connectMongoDB';
 import 'dotenv/config';
+
+import { connectMongoDB } from './db/connectMongoDB.js';
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
-await connectMongoDB();
-
 app.use(express.json());
 app.use(cors());
-app.use(
-  pino({
-    level: 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'HH:MM:ss',
-        ignore: 'pid,hostname',
-        messageFormat:
-          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-        hideObject: true,
-      },
-    },
-  }),
-);
+app.use(logger);
 
 app.use((req, res, next) => {
   console.log(`Time: ${new Date().toLocaleString()}`);
@@ -52,20 +38,14 @@ app.get('/test-error', () => {
 });
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Route not found' });
+  notFoundHandler(req, res);
 });
 
 app.use((err, req, res, next) => {
-  console.error('Error:', err.message);
-
-  const isProd = process.env.NODE_ENV === 'production';
-
-  res.status(500).json({
-    message: isProd
-      ? 'Something went wrong. Please try again later.'
-      : err.message,
-  });
+  errorHandler(err, req, res, next);
 });
+
+await connectMongoDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
