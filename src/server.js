2c5dc8ee678524646dd90448575b0c7d806b0083
d-levchenko <dl.qubb@gmail.com>
2026-07-23 23:15:08 +0300

    Connect mongodb in project

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
+import { connectMongoDB } from './db/connectMongoDB';
 import 'dotenv/config';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
+
+await connectMongoDB();
 
 app.use(express.json());
 app.use(cors());
