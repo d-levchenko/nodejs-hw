bb04ac8d608e2aad621e296defcad4fea5e22c52
d-levchenko <dl.qubb@gmail.com>
2026-07-30 23:03:45 +0300

    feat: added middleware for notes routes; updated controllers

--- src/models/note.js
+++ src/models/note.js
@@ -19,12 +19,17 @@ const noteSchema = new Schema(
       enum: TAGS,
       default: 'Todo',
     },
+    userId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
   },
   {
     timestamps: true,
   },
 );
 
-noteSchema.index({ tag: 1 });
+noteSchema.index({ tag: 1, userId: 1 });
 
 export const Note = model('Note', noteSchema);
